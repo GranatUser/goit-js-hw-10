2d0b83606cb4bc753292edd0b19b418b437151c6
vvv-k <vkorotlov@gmail.com>
2023-01-23 07:11:41 +0300

    Third commit

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,9 +2,6 @@ export default function fetchCountries(url) {
     url = url.trim();
     return fetch(`https://restcountries.com/v3.1/name/${url}?fields=name,capital,population,flags,languages`)
         .then((countries) => {
-            if (url.length === 0) {
-                throw new Error("405");
-            }
             if (!countries.ok) {
                 throw new Error(countries.status);
             }
--- src/index.js
+++ src/index.js
@@ -6,30 +6,34 @@ import fetchCountries from './fetchCountries';
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
-let countriesEl = [];
+let countriesEl = "";
 
 const input = document.getElementById("search-box");
 const ul = document.querySelector(".country-list");
 input.addEventListener("input", debounce((event) => {
-
+    if (event.target.value.trim() === "") {
+        ul.innerHTML = "";
+        return;
+    };
     fetchCountries(event.target.value)
         .then((countries) => {
 
             if (countries.length > 10) {
                 Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-                countriesEl = [];
+                countriesEl = "";
             }
 
-            if (countries.length <= 10 && countries.length >= 2) {
+            else if (countries.length <= 10 && countries.length >= 2) {
                 countriesEl = countries.map((country) => {
                     return `<li class="country-list__item">
             <img width="30" height="30" src=${country.flags.svg}>
             ${country.name.official}
           </li>`
                 }).join("");
+
             }
 
-            if (countries.length === 1) {
+            else if (countries.length === 1) {
                 countriesEl = countries.map((country) => {
                     return `<li><p class="country-list__official-name"> <img width="30" height="30" src=${country.flags.svg}>
             <b> ${country.name.official}</b></p> 
@@ -38,18 +42,14 @@ input.addEventListener("input", debounce((event) => {
             <p><b>Languages</b>: ${Object.values(country.languages).join(", ")}</p> 
             </li>`
                 }).join("");
+
             }
-
             ul.innerHTML = countriesEl;
-
         })
         .catch(error => {
             if (error.message === "404")
                 Notiflix.Notify.failure("Oops, there is no country with that name");
             ul.innerHTML = "";
-            if (error.message === "405") {
-                ul.innerHTML = "";
-            }
         });
 
 }, DEBOUNCE_DELAY));
